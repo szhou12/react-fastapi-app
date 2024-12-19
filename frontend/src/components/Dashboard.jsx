0f084efb59ef35de31d2c112beebde50f8028f4e
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2024-12-18 18:36:42 -0600

    Added: sidebar to Dashboard layout

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -19,6 +19,7 @@ import {
   FcFinePrint,
   FcGlobe,
 } from 'react-icons/fc'
+import Sidebar from './Sidebar'
 
 // interface CardProps {
 //   heading: string
@@ -71,38 +72,41 @@ const LinkCard = ({ heading, description, icon, href }) => {
 
 export default function Dashboard() {
   return (
-    <Box p={4}>
-      <Stack spacing={4} as={Container} maxW={'3xl'} textAlign={'center'}>
-        <Heading fontSize={{ base: '2xl', sm: '4xl' }} fontWeight={'bold'} color={'ui.darkSlate'}>
-          Home
-        </Heading>
-        <Text color={'ui.dim'} fontSize={{ base: 'sm', sm: 'lg' }}>
-          Choose the function you want to access
-        </Text>
-      </Stack>
+    <Flex>
+      <Sidebar />
+      <Box flex="1" p={8}>
+        <Stack spacing={4} maxW={'3xl'} textAlign={'center'}>
+          <Heading fontSize={{ base: '2xl', sm: '4xl' }} fontWeight={'bold'} color={'ui.darkSlate'}>
+            Home
+          </Heading>
+          <Text color={'ui.dim'} fontSize={{ base: 'sm', sm: 'lg' }}>
+            Choose the function you want to access
+          </Text>
+        </Stack>
 
-      <Container maxW={'2xl'} mt={12}>
-        <Flex flexWrap="wrap" gridGap={6} justify="flex-start">
-          <LinkCard
-            heading={'Scraper'}
-            icon={<Icon as={FcGlobe} w={10} h={10} />}
-            description={'Scrape data from the Internet.'}
-            href={'#'}
-          />
-          <LinkCard
-            heading={'Uploader'}
-            icon={<Icon as={FcFinePrint} w={10} h={10} />}
-            description={'Upload file documents to the DB.'}
-            href={'#'}
-          />
-          <LinkCard
-            heading={'Admin'}
-            icon={<Icon as={FcConferenceCall} w={10} h={10} />}
-            description={'Manage currently registered users.'}
-            href={'#'}
-          />
-        </Flex>
-      </Container>
-    </Box>
+        <Box mt={12}>
+          <Flex flexWrap="wrap" gridGap={6} justify="flex-start">
+            <LinkCard
+              heading={'Scraper'}
+              icon={<Icon as={FcGlobe} w={10} h={10} />}
+              description={'Scrape data from the Internet.'}
+              href={'#'}
+            />
+            <LinkCard
+              heading={'Uploader'}
+              icon={<Icon as={FcFinePrint} w={10} h={10} />}
+              description={'Upload file documents to the DB.'}
+              href={'#'}
+            />
+            <LinkCard
+              heading={'Admin'}
+              icon={<Icon as={FcConferenceCall} w={10} h={10} />}
+              description={'Manage currently registered users.'}
+              href={'#'}
+            />
+          </Flex>
+        </Box>
+      </Box>
+    </Flex>
   )
 }
