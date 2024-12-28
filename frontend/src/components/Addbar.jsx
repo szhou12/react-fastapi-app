1e24b019e4d9e85ec7e6cac1d8bbf1dd423fced4
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2024-12-27 18:29:24 -0600

    Added: Addbar

---
+++ frontend/src/components/Addbar.jsx
@@ -0,0 +1,22 @@
+import { Button, Flex, Icon, useDisclosure } from "@chakra-ui/react"
+import { FaPlus } from "react-icons/fa"
+
+
+// type: string
+// addModalAs: React.ComponentType | React.ElementType
+const Addbar = ({ type, addModalAs}) => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const AddModal = addModalAs
+
+    return (
+        <>
+            <Flex py={8} gap={4}>
+                <Button variant="primary" onClick={onOpen} gap={1} fontSize={{ base: "sm", md: "inherit" }}>
+                    <Icon as={FaPlus} /> Add {type}
+                </Button>
+                <AddModal isOpen={isOpen} onClose={onClose} />
+            </Flex>
+        </>
+    )
+
+}
